48c8dfb093c63d5338d0a30f07bb809d5020f785
Saugat Ojha <ojhasaugat81@gmail.com>
2026-04-30 23:53:34 +0545

    learned: Variables in py

---
+++ week01/basics/variables.py
@@ -0,0 +1,11 @@
+stock_price= 233.32  #vairables in py integer,float,string 
+stock_name= 'HRL'
+stock_number=1
+
+stock_price_2=232.31;stock_name_2='HBL' # like c/cpp we can use ; the use of semicolon is to mark end of the line and let the code goto next line here 2 codes are in 1 line ; sperates this
+
+print(stock_price)
+print(stock_name)
+print(stock_number)
+print(stock_price_2)
+print(stock_name_2)
