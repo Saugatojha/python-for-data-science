78ab000e45aa230e715cbbb4302d09dff66bf1ac
Saugat Ojha <ojhasaugat81@gmail.com>
2026-05-02 00:28:32 +0545

    learned boolean, boolean is not only 0 and 1 but truth and falsy with example, and used in real life scenario

--- week01/basics/variables.py
+++ week01/basics/variables.py
@@ -1,4 +1,4 @@
-stock_price= 233.32  #vairables in py integer,float,string 
+stock_price= 233.32  #vairables in py integer, float, string, bool
 stock_name= 'HRL'
 stock_number=1
 
@@ -9,3 +9,26 @@ print(stock_name)
 print(stock_number)
 print(stock_price_2)
 print(stock_name_2)
+
+price_increase = True
+price_decrease = False
+print("___Boolean___")
+print(int(price_increase)) # 1
+print(int(price_decrease)) # 0
+
+print(bool(0)) # False
+print(bool(1)) # True
+
+
+print ("These all act like False:")
+print(bool(0))      # False
+print(bool(0.0))    # False
+print(bool(""))     # False
+print(bool([]))     # False
+print(bool(None))   # False
+
+print("These all act as True")
+print(bool(100))       # True
+print(bool(3.14))      # True
+print(bool("NEPSE"))   # True
+print(bool([1,2,3]))   # True
