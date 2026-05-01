9990788d4349d654ef7d23ad34f52ffc74accb2b
Saugat Ojha <ojhasaugat81@gmail.com>
2026-05-01 22:55:57 +0545

    created varaibles and asked user to give input and displayed output

---
+++ week01/practice/p1.py
@@ -0,0 +1,4 @@
+number_of_shares=input("Enter number of stocks you have: ")
+print(number_of_shares)
+name_of_the_company=input("Enter the name of the company you have: ")
+print(name_of_the_company)
