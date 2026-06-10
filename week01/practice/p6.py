aa9fcf2f87a88b335bfc88c9a0d209e63881b913
Saugat Ojha <ojhasaugat81@gmail.com>
2026-06-10 20:14:41 +0545

    multplicationn table of any number using loop

---
+++ week01/practice/p6.py
@@ -0,0 +1,9 @@
+# wap to Print the multiplication (eg: table of 5 (5x1 to 5x10)).
+number  = int(input("enter a number of which you want multiplication table of: "))
+i= 1
+for i in range(i,11):
+    result = number * i
+    print(result)
+
+
+# 5 number 5*1 = 5 
