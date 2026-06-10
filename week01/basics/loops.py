99be3e8146fbb2492384bdeb27a471c81f635c52
Saugat Ojha <ojhasaugat81@gmail.com>
2026-06-10 19:05:39 +0545

    learned loops using py

---
+++ week01/basics/loops.py
@@ -0,0 +1,7 @@
+# Q1. WAP to print all numbers from 1 to 50, but skip any number that is divisible by both 3 and 5
+for number in range(1, 50):
+    if number % 3 == 0 and number % 5 == 0:
+     print(number)
+    
+    
+    
