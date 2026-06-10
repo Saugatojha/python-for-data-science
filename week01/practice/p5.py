5f4532225d5330023c2dac631e0811ee7e3b4d98
Saugat Ojha <ojhasaugat81@gmail.com>
2026-06-10 20:03:56 +0545

    Used loops to print values inside the list in an iteration for better efficency

---
+++ week01/practice/p5.py
@@ -0,0 +1,9 @@
+# Loop through a list ["apple", "banana", "cherry"] and print each item.
+
+fruits = ["apple", "banana", "cherry"]
+k = 0
+for k in range(k,3):
+    print(k)
+    print(fruits[k])
+
+
